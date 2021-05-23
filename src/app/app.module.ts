7acbaef365ef21c06373b1b9a37448d07b4f1fe4
AndrewJBateman <gomezbateman@yahoo.com>
2021-05-23 07:52:44 +0200

    chore: dependency & formatting updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	entryComponents: [],
+	imports: [
+		BrowserModule,
+		IonicModule.forRoot(),
+		AppRoutingModule,
+		HttpClientModule,
+	],
+	providers: [
+		StatusBar,
+		SplashScreen,
+		{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+	],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/info/info.page.ts
+++ src/app/pages/info/info.page.ts
@@ -4,20 +4,20 @@ import { Observable } from "rxjs";
 import { InfoService, SearchType } from "./../../services/info.service";
 
 @Component({
-  selector: "app-info",
-  templateUrl: "./info.page.html",
-  styleUrls: ["./info.page.scss"],
+	selector: "app-info",
+	templateUrl: "./info.page.html",
+	styleUrls: ["./info.page.scss"],
 })
 export class InfoPage implements OnInit {
-  results: Observable<any>;
-  searchTerm: "";
-  type: SearchType = SearchType.all;
+	results: Observable<any>;
+	searchTerm: "";
+	type: SearchType = SearchType.all;
 
-  constructor(private infoService: InfoService) {}
+	constructor(private infoService: InfoService) {}
 
-  ngOnInit() {}
+	ngOnInit() {}
 
-  searchChanged(): void {
-    this.results = this.infoService.searchData(this.searchTerm, this.type);
-  }
+	searchChanged(): void {
+		this.results = this.infoService.searchData(this.searchTerm, this.type);
+	}
 }
