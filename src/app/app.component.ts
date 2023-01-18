b9573ad22fecd4b4e57e2e72988eee3118067b17
Andrew Bateman <gomezbateman@yahoo.com>
2023-01-18 10:17:56 +0100

    refactor: add template to ts file & self-closing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  template: `<ion-app>
+  <ion-router-outlet />
+</ion-app>`
 })
 export class AppComponent {
   constructor(
